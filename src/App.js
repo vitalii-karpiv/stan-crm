a59c9afc9eec55f59820a68b738c70e2db376eeb
Vitalii Karpiv <vitalia.karpiv@gmail.com>
2024-02-01 14:51:59 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+      <>
+          <div className="App">
+              <div>hello world</div>
+          </div>
+      </>
+  );
+}
+
+export default App;
